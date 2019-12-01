86010fcf586d22c344824405bdb1c9a1ccd19e76
Tim Peters <zomg.tim@gmail.com>
2019-12-01 20:43:40 +1030

    Add day 1 part 2

--- lib/day1.rb
+++ lib/day1.rb
@@ -3,7 +3,20 @@ class Day1 < Base
     mass / 3 - 2
   end
 
+  def total_fuel_required(mass:)
+    fuel = fuel_required(mass: mass)
+    if fuel > 8 # first mass that requires positive fuel itself
+      fuel + total_fuel_required(mass: fuel)
+    else
+      fuel
+    end
+  end
+
   def part1
     input.each_line.map { |line| fuel_required(mass: line.chomp.to_i) }.inject(:+)
+  end
+
+  def part2
+    input.each_line.map { |line| total_fuel_required(mass: line.chomp.to_i) }.inject(:+)
   end
 end
--- test/day1_test.rb
+++ test/day1_test.rb
@@ -6,4 +6,11 @@ class Day1Test < Minitest::Test
     assert_equal 654, d1.fuel_required(mass: 1969)
     assert_equal 33583, d1.fuel_required(mass: 100756)
   end
+
+  def test_total_fuel_required
+    d1 = Day1.new
+    assert_equal 2, d1.total_fuel_required(mass: 14)
+    assert_equal 966, d1.total_fuel_required(mass: 1969)
+    assert_equal 50346, d1.total_fuel_required(mass: 100756)
+  end
 end

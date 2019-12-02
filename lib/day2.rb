4b47d5804b570c01685331b38e0200db72accfae
Tim Peters <zomg.tim@gmail.com>
2019-12-03 07:21:50 +1030

    Refactor day 2

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,25 +1,36 @@
 class Day2 < Base
-  attr_accessor :vm
+  attr_accessor :vm, :pos
 
   def run
-    pos = 0
-    loop do
+    self.pos = 0
+    while vm[pos] != 99
       case vm[pos]
-      when 1 then vm[vm[pos + 3]] = vm[vm[pos + 1]] + vm[vm[pos + 2]]
-      when 2 then vm[vm[pos + 3]] = vm[vm[pos + 1]] * vm[vm[pos + 2]]
-      when 99 then return
+      when 1 then add
+      when 2 then multiply
       else raise "Unexpected opcode at #{pos}: #{vm[pos]}"
       end
-      pos += 4
+      self.pos += 4
     end
+  end
+
+  def add
+    vm[vm[pos + 3]] = vm[vm[pos + 1]] + vm[vm[pos + 2]]
+  end
+
+  def multiply
+    vm[vm[pos + 3]] = vm[vm[pos + 1]] * vm[vm[pos + 2]]
+  end
+
+  def execute(noun, verb)
+    vm[1] = noun
+    vm[2] = verb
+    run
+    vm[0]
   end
 
   def part1
     self.vm = input.chomp.split(/,/).map(&:to_i)
-    vm[1] = 12
-    vm[2] = 2
-    run
-    vm[0]
+    execute(12, 2)
   end
 
   def part2
@@ -27,10 +38,7 @@ class Day2 < Base
     0.upto(99).each do |noun|
       0.upto(99).each do |verb|
         self.vm = initial_state.dup
-        vm[1] = noun
-        vm[2] = verb
-        run
-        return noun * 100 + verb if vm[0] == 19690720
+        return noun * 100 + verb if execute(noun, verb) == 19690720
       end
     end
     raise "Solution not found"

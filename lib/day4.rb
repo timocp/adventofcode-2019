41d8cddfb058df73040d0f3ba7fa17e7a15fd3ac
Tim Peters <zomg.tim@gmail.com>
2019-12-04 18:35:20 +1030

    Add day 4 part 2

--- lib/day4.rb
+++ lib/day4.rb
@@ -1,17 +1,32 @@
 class Day4 < Base
-  def valid?(password)
+  def valid?(password, strict: false)
     return false if password.size != 6
 
-    has_double = false
+    count = Hash.new(0)
     password.chars.each_cons(2) do |a, b|
-      has_double = true if a == b
       return false if b < a
+      count[a] += 1
     end
-    has_double
+    count[password[-1]] += 1
+    if strict
+      # must be at least one which was a double not part of a larger group
+      count.values.detect { |c| c == 2 }
+    else
+      # any double is OK
+      count.values.detect { |c| c > 1 }
+    end
   end
 
   def part1
+    input_to_range.count { |password| valid?(password) }
+  end
+
+  def part2
+    input_to_range.count { |password| valid?(password, strict: true) }
+  end
+
+  def input_to_range
     start, finish = input.chomp.split("-")
-    (start..finish).count { |password| valid?(password) }
+    (start..finish)
   end
 end
--- test/day4_test.rb
+++ test/day4_test.rb
@@ -4,5 +4,12 @@ class Day4Test < Minitest::Test
     assert d.valid?("111111")
     refute d.valid?("223450")
     refute d.valid?("123789")
+    assert d.valid?("123444")
+
+    refute d.valid?("111111", strict: true)
+    assert d.valid?("112233", strict: true)
+    refute d.valid?("123444", strict: true)
+    assert d.valid?("111122", strict: true)
+    refute d.valid?("123333", strict: true)
   end
 end

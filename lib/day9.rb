63f54aef01eed858d0cc85d27aa83ca1d47ae707
Tim Peters <zomg.tim@gmail.com>
2019-12-09 18:46:41 +1030

    Add day 9 part 2

--- lib/day9.rb
+++ lib/day9.rb
@@ -5,6 +5,10 @@ class Day9 < Base
     Intcode.new(program).run(input: [1]).first
   end
 
+  def part2
+    Intcode.new(program).run(input: [2]).first
+  end
+
   def program
     raw_input.split(",").map(&:to_i)
   end

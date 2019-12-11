0cd0b656ceabec614d13cf51d793c749c7e7f882
Tim Peters <zomg.tim@gmail.com>
2019-12-11 18:58:12 +1030

    Add day 11 part 1

---
+++ lib/day11.rb
@@ -0,0 +1,81 @@
+require "intcode"
+
+class Day11 < Base
+  class Robot
+    Point = Struct.new(:x, :y)
+    TURN_LEFT = { up: :left, right: :up, down: :right, left: :down }.freeze
+    TURN_RIGHT = { up: :right, right: :down, down: :left, left: :up }.freeze
+    DELTA = {
+      up: Point.new(1, 0), right: Point.new(0, 1), down: Point.new(-1, 0), left: Point.new(0, -1)
+    }.freeze
+    SHIP = { up: "^", right: ">", down: "v", left: "<" }.freeze
+
+    def initialize
+      @pos = Point.new(0, 0)
+      @facing = :up
+      @hull = Hash.new(0) # key by Point -> 0 = black, 1 = white
+    end
+
+    def execute(program)
+      vm = Intcode.new(program)
+      until vm.terminated
+        commands = vm.run(input: [@hull[@pos]])
+        commands.each_cons(2) do |cmd_colour, cmd_dir|
+          @hull[@pos] = cmd_colour
+          case cmd_dir
+          when 0 then turn_left
+          when 1 then turn_right
+          end
+          move_forward
+        end
+      end
+    end
+
+    def panels_painted
+      @hull.size
+    end
+
+    def hull_to_s
+      minx, maxx = @hull.keys.map(&:x).minmax
+      miny, maxy = @hull.keys.map(&:y).minmax
+      s = ""
+      miny.upto(maxy) do |y|
+        minx.upto(maxx) do |x|
+          s << if @pos.x == x && @pos.y == y
+                 SHIP[@facing]
+               else
+                 @hull[Point.new(x, y)].to_s
+               end
+        end
+        s += "\n"
+      end
+      s
+    end
+
+    private
+
+    def turn_left
+      @facing = TURN_LEFT[@facing]
+    end
+
+    def turn_right
+      @facing = TURN_RIGHT[@facing]
+    end
+
+    def move_forward
+      @pos = Point.new(@pos.x + DELTA[@facing].x, @pos.y + DELTA[@facing].y)
+    end
+  end
+
+  def part1
+    robot = Robot.new
+    robot.execute(input)
+    robot.panels_painted
+  end
+
+  private
+
+  def input
+    raw_input.split(",").map(&:to_i)
+  end
+end
---
+++ test/day11_test.rb
@@ -0,0 +1,4 @@
+require "day11"
+
+class Day11Test < Minitest::Test
+end

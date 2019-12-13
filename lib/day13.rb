95e45bc7a425371f495f90e1340f08e769663a76
Tim Peters <zomg.tim@gmail.com>
2019-12-13 18:58:21 +1030

    Add day 13 part 2

--- lib/day13.rb
+++ lib/day13.rb
@@ -4,10 +4,21 @@ class Day13 < Base
   class Screen
     def initialize
       @screen = Hash.new(0)
+      @score = 0
+      @paddle = nil
+      @ball = nil
     end
 
+    attr_reader :score, :paddle, :ball
+
     def draw(x, y, tile_id)
-      @screen[[x, y]] = tile_id
+      if x == -1
+        @score = tile_id
+      else
+        @screen[[x, y]] = tile_id
+        @paddle = x if tile_id == 3
+        @ball = x if tile_id == 4
+      end
     end
 
     def inspect
@@ -24,7 +35,7 @@ class Day13 < Base
         end
         s += "\n"
       end
-      s
+      s + "P1 #{@score}\n"
     end
   end
 
@@ -35,6 +46,20 @@ class Day13 < Base
       screen.draw(x, y, tile_id)
     end
     screen.inspect.count("B")
+  end
+
+  def part2
+    screen = Screen.new
+    vm = Intcode.new(program.tap { |p| p[0] = 2 })
+    until vm.terminated
+      input = []
+      input << (screen.ball <=> screen.paddle) if screen.ball
+      vm.run(input: input).each_slice(3) do |x, y, tile_id|
+        screen.draw(x, y, tile_id)
+      end
+      # puts screen.inspect # uncomment to watch game
+    end
+    screen.score
   end
 
   def program

4e8b903abd727e7b84446a07a65a11f53a516142
Tim Peters <zomg.tim@gmail.com>
2019-12-16 19:30:34 +1030

    Speed-up a bit

--- lib/day16.rb
+++ lib/day16.rb
@@ -2,12 +2,15 @@ class Day16 < Base
   PATTERN = [0, 1, 0, -1].freeze
 
   def phase(input)
-    input.map.with_index(1) do |_, rownum|
-      input.map.with_index(1) do |digit, colnum|
-        offset = (colnum / rownum)
-        digit * PATTERN[offset % 4]
-      end.inject(&:+).abs % 10
+    output = Array.new(input.length)
+    (1..(input.length)).each do |rownum|
+      new_digit = 0
+      input.each.with_index(1) do |digit, colnum|
+        new_digit += digit * PATTERN[(colnum / rownum) % 4]
+      end
+      output[rownum - 1] = new_digit.abs % 10
     end
+    output
   end
 
   def part1
